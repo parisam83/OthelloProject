cbd452fcb299991c236740579fcf422001f90d7f
Parisa <mousaviparisa5@yahoo.com>
2023-04-21 17:25:15 +0330

    Create Piece.java model

---
+++ src/main/java/model/Piece.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Piece {
+    final int x, y;
+    boolean color;
+
+    public Piece(int x, int y, boolean color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
